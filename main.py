0dee306d35ea96abf49abfe10009dc5057aa5653
Александр <mam0ru@mail.ru>
2024-09-10 19:42:10 +0700

    yo

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('asdv')
+# text: str = ''
+# badword: list[str] = []
+
+# with open('input.txt', encoding='utf-8') as f:
+#     text = f.read()
+
+# with open("badword.txt", encoding='utf-8') as f:
+#     badword = f.read().split(' ')
+
+# for word in badword:
+#     text = text.replace(word, len(word) * '*')
+
+# with open('output.txt', 'w', encoding='utf-8') as f:
+#     f.write(text)
+
+
+
+
+
