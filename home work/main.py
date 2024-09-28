5680b6ab8706b84446ee21f46190c85e02894909
Александр <mam0ru@mail.ru>
2024-09-28 16:26:47 +0700

    задание 3

--- home work/main.py
+++ home work/main.py
@@ -36,34 +36,53 @@
 
 
 
+# fail:str = ''
+# with open('home work/input.txt', encoding='utf-8') as f:
+#     fail = f.read()
+
+# fail_str: list = fail.split('\n')
+# count_symbol = 0
+# count_string = len(fail_str)
+# count_v_letter = 0
+# count_c_letter = 0
+# count_num = 0
+
+# for i in fail:
+#     count_symbol += 1
+
+# for i in fail:
+#     i = str(i.upper())
+#     vowels = 'УЕЫАОЭИЮ'
+#     consonant = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩ"
+#     num = '1234567890'
+#     if i in vowels:
+#         count_v_letter += 1
+#     elif i in consonant:
+#         count_c_letter += 1
+#     elif i in num:
+#         count_num += 1
+
+# result: str = f'Количество символов {count_symbol}\nКоличество строк {count_string}\nКоличество глассных букв {count_v_letter}\nКоличество согласных букв {count_c_letter}\nКоличество цифр {count_num}'
+# print(result)
+
+# with open('home work/output.txt','w',encoding='utf-8') as f:
+#     f.write(result)
+
+
+
+# Задание 3
+# Дан текстовый файл. Удалить из него последнюю
+# строку. Результат записать в другой файл.
+
+
 fail:str = ''
-with open('home work/input.txt', encoding='utf-8') as f:
+
+with open('home work/input.txt',encoding='utf-8') as f:
     fail = f.read()
 
-fail_str: list = fail.split('\n')
-count_symbol = 0
-count_string = len(fail_str)
-count_v_letter = 0
-count_c_letter = 0
-count_num = 0
-
-for i in fail:
-    count_symbol += 1
-
-for i in fail:
-    i = str(i.upper())
-    vowels = 'УЕЫАОЭИЮ'
-    consonant = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩ"
-    num = '1234567890'
-    if i in vowels:
-        count_v_letter += 1
-    elif i in consonant:
-        count_c_letter += 1
-    elif i in num:
-        count_num += 1
-
-result: str = f'Количество символов {count_symbol}\nКоличество строк {count_string}\nКоличество глассных букв {count_v_letter}\nКоличество согласных букв {count_c_letter}\nКоличество цифр {count_num}'
-print(result)
-
+fail_arr: list = fail.split('\n')
+fail_arr.remove(fail_arr[-1])
+print(fail_arr)
+fail_arr = ('\n').join(fail_arr)
 with open('home work/output.txt','w',encoding='utf-8') as f:
-    f.write(result)
+    f.write(fail_arr)
