55fd997842761b1f96c01f8842c720d5090e5ccd
Александр <mam0ru@mail.ru>
2024-10-01 23:41:37 +0700

    Задание 2

--- home work3/main.py
+++ home work3/main.py
@@ -4,19 +4,61 @@
 
 
 
-class Fraction:
-    __count = 0
+# class Fraction:
+#     __count = 0
 
-    def __init__(self,dividend,divider):
-        self._dividend: int = dividend
-        self._divider: int = divider
-        Fraction.__count += 1
+#     def __init__(self,dividend,divider):
+#         self._dividend: int = dividend
+#         self._divider: int = divider
+#         Fraction.__count += 1
 
+
+#     @staticmethod
+#     def countFraction() -> int:
+#         return Fraction.__count
+
+# fr = Fraction(1,4)
+# fr1 = Fraction(2,3)
+# print(Fraction.countFraction())
+
+
+
+
+
+# Задание 2
+# Создайте класс для конвертирования температуры из
+# Цельсия в Фаренгейт и наоборот. У класса должно быть
+# два статических метода: для перевода из Цельсия в Фаренгейт
+# и для перевода из Фаренгейта в Цельсий. Также
+# класс должен считать количество подсчетов температурыи
+# возвращать это значение с помощью статического метода.
+
+
+
+class Сonverter:
+    count = 0
+    
+    def __init__(self,num: int):
+        self._num: int = num
+
+    def __str__(self) -> str:
+        return f'{self._num}'
+    
+    @staticmethod
+    def cel_in_far(num:int) -> int:
+        Сonverter.count += 1
+        return (num) * 1.8 + 32
+    
+    @staticmethod
+    def far_in_cel(num:int) -> int:
+        Сonverter.count += 1
+        return (num - 32) / 1.8
 
     @staticmethod
-    def countFraction() -> int:
-        return Fraction.__count
+    def countConv():
+        return f'{Сonverter.count}'
 
-fr = Fraction(1,4)
-fr1 = Fraction(2,3)
-print(Fraction.countFraction())
+
+print(Сonverter.cel_in_far(10))
+print(Сonverter.far_in_cel(50))
+print(Сonverter.countConv())
