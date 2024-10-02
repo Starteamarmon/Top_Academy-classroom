817b05d08bc7c84a7f068b5b44db9349497504d1
Александр <mam0ru@mail.ru>
2024-10-02 19:05:33 +0700

    задание 2

--- home work3/main2.py
+++ home work3/main2.py
@@ -9,11 +9,15 @@ class Figure:
     def __init__(self, side: int):
         self._side: int = side
 
+
 class Round(Figure):
     def __init__(self, side: int):
         super().__init__(side)
 
-    def s(self):
+    def __int__(self) -> int:
+        return int(3.14 * self._side**2)
+
+    def __str__(self):
         return f'Площать круга при радиусе в {self._side} равна {3.14 * self._side**2}'
     
 class Tiangle(Figure):
@@ -21,7 +25,10 @@ class Tiangle(Figure):
         super().__init__(side)
         self._side1: int = side1
     
-    def s(self):
+    def __int__(self) -> int:
+        return int((self._side * self._side1)/2)
+
+    def __str__(self):
         return f'Площадь треугольника при катетах {self._side} и {self._side1} равна {(self._side * self._side1)/2}'
 
 
@@ -30,7 +37,10 @@ class Rectangle(Figure):
         super().__init__(side)
         self._side1: int = side1
 
-    def s(self):
+    def __int__(self) -> int:
+        return int(self._side * self._side1)
+
+    def __str__(self):
         return f'Площадь прямоугольника при сторонах {self._side} и {self._side1} равна {self._side * self._side1}'
 
 
@@ -40,18 +50,35 @@ class Trapezoid(Figure):
         self._side1: int = side1
         self._height: int = height
     
+    def __int__(self) -> int:
+        return int(((self._side + self._side1) / 2) * self._height)
 
-    def s(self):
+    def __str__(self):
         return f'Площадь трапеции при параллельных сторонах {self._side}, {self._side1} и высоты {self._height} равна {((self._side + self._side1) / 2) * self._height}'
 
 ronud = Round(3)
-print(ronud.s())
+print(ronud)
+ronud1 = int(ronud)
+print(ronud1)
 
 tiangle = Tiangle(10,15)
-print(tiangle.s())
+print(tiangle)
+tiangle1 = int(tiangle)
+print(tiangle1)
 
 rectangle = Rectangle(3,5)
-print(rectangle.s())
+print(rectangle)
+rectangle1 = int(rectangle)
+print(rectangle1)
 
 trapezoid = Trapezoid(3,5,7)
-print(trapezoid.s())
+print(trapezoid)
+trapezoid1 = int(trapezoid)
+print(trapezoid1)
+
+
+# Задание 2
+# Для классов из задания 1 нужно переопределить магические методы int(возвращает площадь) и str(возвращает
+# информацию о фигуре).
+
+
