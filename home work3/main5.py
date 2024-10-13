7251858ba0aeb7d49460a0632cd3789102576b81
Александр <mam0ru@mail.ru>
2024-10-13 19:15:05 +0700

    Задача 6 | 2

--- home work3/main5.py
+++ home work3/main5.py
@@ -28,3 +28,50 @@ a = ComplexNumber(70,88)
 b = ComplexNumber (30,12)
 print(a + b)
 print(a - b)
+
+
+# Задача 6: Инкапсуляция
+
+# Описание: Создайте класс Car, который содержит информацию о марке автомобиля,
+#  максимальной скорости и текущей скорости. Инкапсулируйте переменные с текущей скоростью,
+#  чтобы нельзя было напрямую её изменять.
+
+# Условия:
+
+#  • Создайте конструктор, принимающий марку и максимальную скорость.
+#  • Создайте методы для увеличения и уменьшения скорости, контролируя, чтобы скорость не превышала максимальную.
+#  • Добавьте метод для отображения текущей скорости.
+
+class Car:
+    def __init__(self,brand: str ,max_speed: int, current_speed: int = 0):
+        self.__brand: str = brand
+        self.__max_speed: int = max_speed
+        self.__current_speed: int = current_speed
+    
+    def gas(self,gas):
+        if gas + self.__current_speed < self.__max_speed:
+            self.__current_speed = gas + self.__current_speed
+            return self.__current_speed
+        else:
+            self.__current_speed = self.__max_speed
+            return self.__current_speed
+        
+    def brake(self,brake):
+        if self.__current_speed - brake > 0:
+            self.__current_speed = self.__current_speed - brake
+        else:
+            self.__current_speed = 0
+            return self.__current_speed
+        
+    def __str__(self):
+        return f'Машина {self.__brand} двигается со скоростью {self.__current_speed} км/ч и имеет максимальную скорость {self.__max_speed}'
+    
+
+car = Car('Интегра', 220)
+print(car)
+car.gas(270)
+print(car)
+car.brake(30)
+print(car)
+car.brake(200)
+print(car)
